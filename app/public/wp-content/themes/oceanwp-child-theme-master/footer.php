29699d741df69fa77098f6a0f28c0f1c0bed52a5
Gregdevfree <gregory.goix@gmail.com>
2024-09-29 20:48:43 +0200

    finalisation intégration canettes footer et avant réglages responsive

--- app/public/wp-content/themes/oceanwp-child-theme-master/footer.php
+++ app/public/wp-content/themes/oceanwp-child-theme-master/footer.php
@@ -11,17 +11,30 @@
 
 
 <?php
+
 // Footer pour la page 'home'
 if (is_page('home')) {
-    echo '<div class="footer-home"><h2>Pied de page pour la page home</h2>';
-    echo '<div class="footer-container"><p class="footer-text"><a href="' . home_url() . '">Mentions légales</a></p>';
+    echo '<div class="footer-home"><ol class="image-list">';
+    $imageSrc = get_stylesheet_directory_uri() . '/assets/Planty6.png'; // Lien relatif vers l'image
+    for ($i = 1; $i <= 16; $i++) {
+        echo "<li><img src=\"$imageSrc\" alt=\"Image $i\"></li>";
+    }
+    echo '</ol></div>'; // Fermeture de la div footer-home
+    echo '<div class="footer-container"><p class="footer-text"><a href="' . home_url() . '">Mentions légales</a></p></div>'; // Fermeture de la div footer-container
 }
+
 
 // Footer pour la page 'nous-rencontrer'
 if (is_page('nous-rencontrer')) {
-    echo '<div class="footer-nous-rencontrer"><h2>Pied de page pour la page nous-rencontrer</h2>';
-    echo '<div class="footer-container"><p class="footer-text"><a href="' . home_url() . '">Mentions légales</a></p>';
+    echo '<div class="footer-nous-rencontrer"><ol class="image-list">';
+    $imageSrc = get_stylesheet_directory_uri() . '/assets/Planty6.png'; // Lien relatif vers l'image
+    for ($i = 1; $i <= 16; $i++) {
+        echo "<li><img src=\"$imageSrc\" alt=\"Image $i\"></li>";
+    }
+    echo '</ol></div>'; // Fermeture de la div footer-nous-rencontrer
+    echo '<div class="footer-container"><p class="footer-text"><a href="' . home_url() . '">Mentions légales</a></p></div>'; // Fermeture de la div footer-container
 }
+
 
 // Footer pour la page 'commander'
 if (is_page('commander')) {
