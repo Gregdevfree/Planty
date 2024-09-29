504b96362d67c2f6eb984fe0fa6f1684a4ff8a57
Gregdevfree <gregory.goix@gmail.com>
2024-09-29 13:20:01 +0200

    avant intégration canettes footer et réglages responsive

--- app/public/wp-content/themes/oceanwp-child-theme-master/footer.php
+++ app/public/wp-content/themes/oceanwp-child-theme-master/footer.php
@@ -9,10 +9,25 @@
 
 	</main><!-- #main -->
 
-<footer class="footer-menu">
-    <div class="footer-container">
-        <p class="footer-text"><a href="<?php echo home_url(); ?>">Mentions légales</a></p>
-    </div>
+
+<?php
+// Footer pour la page 'home'
+if (is_page('home')) {
+    echo '<div class="footer-home"><h2>Pied de page pour la page home</h2>';
+    echo '<div class="footer-container"><p class="footer-text"><a href="' . home_url() . '">Mentions légales</a></p>';
+}
+
+// Footer pour la page 'nous-rencontrer'
+if (is_page('nous-rencontrer')) {
+    echo '<div class="footer-nous-rencontrer"><h2>Pied de page pour la page nous-rencontrer</h2>';
+    echo '<div class="footer-container"><p class="footer-text"><a href="' . home_url() . '">Mentions légales</a></p>';
+}
+
+// Footer pour la page 'commander'
+if (is_page('commander')) {
+    echo '<div class="footer-container"><p class="footer-text"><a href="' . home_url() . '">Mentions légales</a></p>';
+}
+?>
     <?php wp_footer(); ?>
 </footer>
 </body>
